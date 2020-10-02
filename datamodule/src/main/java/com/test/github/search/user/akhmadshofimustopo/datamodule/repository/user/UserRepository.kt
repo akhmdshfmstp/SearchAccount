f7eba235a937e578a4cec56c0d95afc2a0b4a9f9
akhmadmustopo <akhmad.mustopo@metraplasa.co.id>
2020-10-02 21:43:10 +0700

    add repository user service

---
+++ datamodule/src/main/java/com/test/github/search/user/akhmadshofimustopo/datamodule/repository/user/UserRepository.kt
@@ -0,0 +1,30 @@
+package com.test.github.search.user.akhmadshofimustopo.datamodule.repository.user
+
+import com.test.github.search.user.akhmadshofimustopo.datamodule.service.user.UserService
+import com.test.github.user.finder.datamodule.model.dto.user.UserDto
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+
+interface UserRepository {
+
+    fun getSearchListUserAsync(
+        keyword: String,
+        perPage: Int,
+        page: Int
+    ): Deferred<Response<UserDto>>
+
+}
+
+class UserRepositoryImpl(private val userService: UserService) : UserRepository {
+
+    override fun getSearchListUserAsync(
+        keyword: String,
+        perPage: Int,
+        page: Int
+    ) = userService.getSearchListUserAsync(
+        keyword,
+        perPage,
+        page
+    )
+
+}
