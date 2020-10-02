92effe2ee00a2c5bab452f7039dcafe8415c3059
akhmadmustopo <akhmad.mustopo@metraplasa.co.id>
2020-10-02 20:32:52 +0700

    add core view layout for binding layout

---
+++ framework/src/main/java/com/test/github/search/user/akhmadshofimustopo/framework/core/annotation/ViewLayout.kt
@@ -0,0 +1,9 @@
+package com.test.github.search.user.akhmadshofimustopo.framework.core.annotation
+
+import android.view.View
+import androidx.annotation.LayoutRes
+import androidx.annotation.NonNull
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ViewLayout(@LayoutRes @NonNull val value: Int = View.NO_ID)
