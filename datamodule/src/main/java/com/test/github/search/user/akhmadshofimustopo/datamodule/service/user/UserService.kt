b2ad94d06c2450c203300272996cdf0a6274cead
akhmadmustopo <akhmad.mustopo@metraplasa.co.id>
2020-10-02 21:42:01 +0700

    add user service

---
+++ datamodule/src/main/java/com/test/github/search/user/akhmadshofimustopo/datamodule/service/user/UserService.kt
@@ -0,0 +1,18 @@
+package com.test.github.search.user.akhmadshofimustopo.datamodule.service.user
+
+import com.test.github.user.finder.datamodule.model.dto.user.UserDto
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface UserService {
+
+    @GET("search/users")
+    fun getSearchListUserAsync(
+        @Query("q") keyword: String?,
+        @Query("per_page") perPage: Int?,
+        @Query("page") page: Int?
+    ): Deferred<Response<UserDto>>
+
+}
