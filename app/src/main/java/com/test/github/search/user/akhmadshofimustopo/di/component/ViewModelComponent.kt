04a0f4c98e0fdb390a8fae187ed8dd761d7134af
akhmadmustopo <akhmad.mustopo@metraplasa.co.id>
2020-10-03 20:14:34 +0700

    add vm to component file

---
+++ app/src/main/java/com/test/github/search/user/akhmadshofimustopo/di/component/ViewModelComponent.kt
@@ -0,0 +1,8 @@
+package com.test.github.search.user.akhmadshofimustopo.di.component
+
+import com.test.github.search.user.akhmadshofimustopo.di.module.viewmodel.viewModelModule
+import org.koin.core.module.Module
+
+val viewModelComponent: List<Module> = listOf(
+    viewModelModule
+)
